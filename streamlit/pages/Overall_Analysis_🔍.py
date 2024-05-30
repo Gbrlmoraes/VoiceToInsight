a37879915b62e1fc0c9ceb38aa5a3b2d699cadfa
Gabriel Moraes <gabrielmoraesmagalhaes@gmail.com>
2024-05-29 22:27:32 -0300

    renaming the file and finishing the first version of the home page

---
+++ streamlit/pages/Overall_Analysis_🔍.py
@@ -0,0 +1,14 @@
+# Importing packages
+import streamlit as st
+
+# Page config
+st.set_page_config(
+    page_title = "Overall",
+    page_icon = "🔍",
+)
+
+st.title('Overall Analysis')
+
+st.image(
+    r"C:\Users\Gbrlm\OneDrive\Imagens\agradecimentos.png",
+)
