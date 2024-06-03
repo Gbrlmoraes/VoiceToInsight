7eeca42f49b39e0e720f46cc104976ed0c5c079a
Gabriel Moraes <gabrielmoraesmagalhaes@gmail.com>
2024-06-03 17:15:36 -0300

    working on dataviz

--- streamlit/pages/Overall_Analysis_🔍.py
+++ streamlit/pages/Overall_Analysis_🔍.py
@@ -1,7 +1,9 @@
-# Importing packages
+# Bibliotecas que foram usadas
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-# Page config
+# Configuração da página
 st.set_page_config(
     page_title = "Overall",
     page_icon = "🔍",
@@ -9,6 +11,37 @@ st.set_page_config(
 
 st.title('Overall Analysis')
 
-st.image(
-    r"C:\Users\Gbrlm\OneDrive\Imagens\agradecimentos.png",
+# Import dos dados
+st.cache_data()
+def load_data():
+    return pd.read_csv(r'J:\Estudo\Projetos\VoiceToInsight\data\nps_data.csv')
+
+df = load_data()
+
+# Criando colunas onde ficarão os Cards
+#col1, col2 = st.columns([4, 1])
+
+# Criando dataframe com a quantidade por status
+df_status = pd.DataFrame(df['status'].value_counts()).reset_index()
+df_status.columns = ['Status', 'Quantidade']
+df_status['cor'] = df_status['Status'].map({'Detrator' : '#EE4E4E', 'Neutro' : '#F6EEC9', 'Promotor' : '#A1DD70'})
+
+# Criando gráfico de barras com streamlit
+st.bar_chart(
+    data = df_status,
+    x = 'Status',
+    y = 'Quantidade',
+    color = 'cor',
+    width = 12
+    )
+
+# Criar o gráfico de rosca
+fig = px.pie(
+    df_status, 
+    values = 'Quantidade',
+    names = 'Status',
+    hole = 0.5,
+    color_discrete_sequence = ['#A1DD70', '#F6EEC9' , '#EE4E4E'],
+    category_orders = {"Status": ["Promotor", "Neutro", "Detrator"]}
 )
+st.plotly_chart(fig)
